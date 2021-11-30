441b6b0c227cfcab86243dc455b44c36430c9b4b
AiEngineerSy <90108678+AiEngineerSy@users.noreply.github.com>
2021-11-30 22:48:59 +0200

    sync

--- database/migrations/2020_10_08_090327_create_request_types_table.php
+++ database/migrations/2020_10_08_090327_create_request_types_table.php
@@ -16,6 +16,7 @@ class CreateRequestTypesTable extends Migration
         Schema::create('request_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            
             $table->timestamps();
         });
     }
