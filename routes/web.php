df5c99af26bc5db77df78cc72a0ccf67a7a5ac7b
AiEngineerSy <90108678+AiEngineerSy@users.noreply.github.com>
2021-12-20 23:40:30 +0200

    sync

--- routes/web.php
+++ routes/web.php
@@ -193,23 +193,23 @@ Route::prefix('client')
 
 // Localization
 Route::get('/js/lang.js', function () {
-    $strings = Cache::remember('lang.js', 2, function () {
+   // $strings = Cache::remember('lang.js', 2, function () {
         $lang = config('app.locale');
-
+   
         $files = glob(resource_path('lang/'.$lang.'/*.php'));
        
         $strings = [];
-   
         foreach ($files as $file) {
         
             $name = basename($file, '.php');
-     
+   
             $strings[$name] = require $file;
+         
        
         }
      
-        return $strings;
-    });
+     //   return $strings;
+    //});
 
     header('Content-Type: text/javascript');
     echo 'window.i18n = '.json_encode($strings).';';
